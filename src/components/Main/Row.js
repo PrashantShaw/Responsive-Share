87047d345dbb5a5224f09a21a3df0e5f8f85c4c9
PrashantShaw <prashant.shaw.3154@gmail.com>
2022-12-18 15:08:06 +0530

    fixed a bug

--- src/components/Main/Row.js
+++ src/components/Main/Row.js
@@ -19,14 +19,14 @@ function Row({ userData }) {
     return (
         <div className='oslash'>
             <div className='row-box'>
-                <img className='web-img' src={require(`../Images/${userData.image}`)} />
+                <img className='web-img' src={require(`../Images/${userData.image}`)} alt='user data' /> 
                 <p>{userData.name} <br />
                     <span className='txt-light'>{userData.email}</span> </p>
             </div>
             <div className='drop-down'>
                 <button
                     onClick={() => setDropdown(prev => !prev)}
-                >{userData.access} <img src={require('../Images/dropdown.png')} /></button>
+                >{userData.access} <img src={require('../Images/dropdown.png')} alt='drop down' /></button>
                 <div
                     style={dropdown ? { display: 'block' } : {}}
                     className='dropdown-contents'>
